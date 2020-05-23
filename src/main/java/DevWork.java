3874cef0db5bd11ddeeb58977390483d902bbbe9
WSY <757843031@qq.com>
2020-05-23 21:45:39 +0800

    add a new method called DevWorkByWSY

--- src/main/java/DevWork.java
+++ src/main/java/DevWork.java
@@ -2,4 +2,7 @@ public class DevWork {
     public static void main(String[] args) {
         System.out.println("DevWork");
     }
+    public static void DevWorkByWSY() {
+        System.out.println("DevWorkByWSY");
+    }
 }
